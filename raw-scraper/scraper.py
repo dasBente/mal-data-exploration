4d8d6a8aeb7e79165719b1c6645ce9eb95263530
dasbente@gmail.com <dasbente@gmail.com>
2024-04-05 11:34:22 +0200

    Fixes
    Add missing requirement and fix scraping script.

--- raw-scraper/scraper.py
+++ raw-scraper/scraper.py
@@ -17,14 +17,16 @@ id = 1
 success = True
 
 while True:
-    res = requests.get(f"https://api.jikan.moe/v4/anime/{id}/full")
+    res = requests.get(f"http://api.jikan.moe/v4/anime/{id}/full")
     if res.status_code != 200: break
     
-    anime_data = res.json()
+    anime_data = res.json()["data"]
+    print(f"Downloading {anime_data['title']} with ID {id}")
     sleep(1)
     
     for tag in ["characters", "staff", "statistics", "moreinfo", "recommendations"]:
-        res = requests.get(f"https://api.jikan.moe/v4/anime/{id}/{tag}")
+        print(f"Downloading {tag}")
+        res = requests.get(f"http://api.jikan.moe/v4/anime/{id}/{tag}")
         anime_data[tag] = res.json()["data"] if res.status_code == 200 else []
         sleep(1)
     
@@ -34,19 +36,20 @@ while True:
         items = []
         
         while True:
-            res = requests.get(f"https://api.jikan.moe/v4/anime/{id}/{tag}?page={i}")
+            print(f"Downloading {tag}, page {i}")
+            
+            res = requests.get(f"http://api.jikan.moe/v4/anime/{id}/{tag}?page={i}")
+            tag_data = res.json()            
             sleep(1)
             
-            if res.status_code != 200:
+            if res.status_code != 200 or not "data" in tag_data or len(tag_data["data"]) == 0:
                 anime_data[tag] = items
+                print(f"[{tag}] Stopped with {res.status_code} for {str(tag_data)} on page {i}")
                 break
-        
-            tag_data = res.json()
+            
             items += tag_data["data"]
-            
-            if not tag_data["pagination"]["has_next_page"]:
-                anime_data[tag] = items
-                break
-            
-    db["anime_raw"].insert_one(anime_data)
+            i += 1
+    
+    db["anime_raw"].update_one({"mal_id": id}, {"$set": anime_data}, True)
+    id += 1
     
